47c2af409e3a9ecd621eda6136e083037dd02253
Jessica Pereira <jcpereira070@gmail.com>
2020-06-20 23:48:30 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,6 +2,8 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  key_for_min_value[hash]
+  key_for_min_value = {:blake => 500, :ashley => 2, :adam => 1}
+key_for_min_value.collect do |name|
+  age
 
 end
